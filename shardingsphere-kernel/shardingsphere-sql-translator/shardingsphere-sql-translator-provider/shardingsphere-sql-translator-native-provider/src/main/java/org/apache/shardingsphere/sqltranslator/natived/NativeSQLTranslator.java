2633a433be362a0cdbef88da7a1df0c7bbfb87e4
windwheel <yuluoxinsheng@gmail.com>
2022-05-24 11:22:34 +0800

    [issue-17619](https://github.com/apache/shardingsphere/issues/17619) support native translate todo modify visit logic

--- shardingsphere-kernel/shardingsphere-sql-translator/shardingsphere-sql-translator-provider/shardingsphere-sql-translator-native-provider/src/main/java/org/apache/shardingsphere/sqltranslator/natived/NativeSQLTranslator.java
+++ shardingsphere-kernel/shardingsphere-sql-translator/shardingsphere-sql-translator-provider/shardingsphere-sql-translator-native-provider/src/main/java/org/apache/shardingsphere/sqltranslator/natived/NativeSQLTranslator.java
@@ -17,9 +17,34 @@
 
 package org.apache.shardingsphere.sqltranslator.natived;
 
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+import org.apache.calcite.avatica.util.Casing;
+import org.apache.calcite.config.CalciteConnectionConfig;
+import org.apache.calcite.config.CalciteConnectionConfigImpl;
+import org.apache.calcite.config.CalciteConnectionProperty;
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.parser.SqlParser;
+import org.apache.calcite.sql.parser.impl.SqlParserImpl;
 import org.apache.shardingsphere.infra.database.type.DatabaseType;
+import org.apache.shardingsphere.infra.database.type.DatabaseTypeFactory;
+import org.apache.shardingsphere.infra.federation.optimizer.context.parser.dialect.OptimizerSQLDialectBuilderFactory;
+import org.apache.shardingsphere.infra.federation.optimizer.converter.SQLNodeConverterEngine;
+import org.apache.shardingsphere.sql.parser.api.CacheOption;
+import org.apache.shardingsphere.sql.parser.api.SQLParserEngine;
+import org.apache.shardingsphere.sql.parser.api.SQLVisitorEngine;
+import org.apache.shardingsphere.sql.parser.api.parser.SQLParser;
+import org.apache.shardingsphere.sql.parser.core.ParseASTNode;
+import org.apache.shardingsphere.sql.parser.core.SQLParserFactory;
+import org.apache.shardingsphere.sql.parser.core.database.parser.DatabaseTypedSQLParserFacadeFactory;
+import org.apache.shardingsphere.sql.parser.core.database.visitor.SQLVisitorFactory;
+import org.apache.shardingsphere.sql.parser.core.database.visitor.SQLVisitorRule;
+import org.apache.shardingsphere.sql.parser.spi.DatabaseTypedSQLParserFacade;
 import org.apache.shardingsphere.sql.parser.sql.common.statement.SQLStatement;
+import org.apache.shardingsphere.sql.parser.sql.dialect.statement.mysql.MySQLStatement;
 import org.apache.shardingsphere.sqltranslator.spi.SQLTranslator;
+
+import java.util.Properties;
 
 /**
  * Native SQL translator.
@@ -28,12 +53,35 @@ public final class NativeSQLTranslator implements SQLTranslator {
     
     @Override
     public String translate(final String sql, final SQLStatement statement, final DatabaseType frontendDatabaseType, final DatabaseType backendDatabaseType) {
-        // TODO
-        return sql;
+
+        CacheOption cacheOption = new CacheOption(128, 1024L, 4);
+        SQLParserEngine parserEngine = new SQLParserEngine(frontendDatabaseType.getType(), cacheOption);
+        ParseASTNode parseASTNode = parserEngine.parse(sql, false);
+        SQLVisitorEngine visitorEngine = new SQLVisitorEngine(backendDatabaseType.getType(), "STATEMENT", false, new Properties()); ;
+
+        //FIXME support translate
+        SQLStatement sqlStatement = visitorEngine.visit(parseASTNode);
+        SqlNode actual = SQLNodeConverterEngine.convertToSQLNode(sqlStatement);
+        return actual.toString();
     }
     
     @Override
     public String getType() {
         return "NATIVE";
     }
+
+
+    private SqlParser.Config createConfig(final DatabaseType databaseType) {
+        CalciteConnectionConfig connectionConfig = new CalciteConnectionConfigImpl(createSQLDialectProperties(databaseType));
+        return SqlParser.config().withLex(connectionConfig.lex()).withUnquotedCasing(Casing.UNCHANGED)
+                .withIdentifierMaxLength(SqlParser.DEFAULT_IDENTIFIER_MAX_LENGTH).withConformance(connectionConfig.conformance()).withParserFactory(SqlParserImpl.FACTORY);
+    }
+
+    private Properties createSQLDialectProperties(final DatabaseType databaseType) {
+        Properties result = new Properties();
+        result.setProperty(CalciteConnectionProperty.TIME_ZONE.camelName(), "UTC");
+        result.putAll(OptimizerSQLDialectBuilderFactory.build(databaseType));
+        return result;
+    }
+
 }
